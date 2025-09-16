4917d7d74bd5e7eb368f6ca32cb6ffdc9708bb7c
MohammedYaseenkalvium <m.yaseensayed09@gmail.com>
2025-09-16 09:43:01 +0530

    login is completed in the frontend

--- Frontend/TaskManagerDemo/src/components/layouts/AuthLayouts.jsx
+++ Frontend/TaskManagerDemo/src/components/layouts/AuthLayouts.jsx
@@ -1,19 +1,19 @@
-// import React from 'react';
-// import UI_IMG from "../../assets/images/auth-img.png";
+import React from 'react';
+import UI_IMG from "../../assets/images/auth-img.png";
 
-// const AuthLayout = ({children}) => {
-//   return (
-//     <div className='flex h-screen'>
-//       <div className='w-screen md:w-3/5 px-12 pt-8 pb-12'>
-//         <h2 className='text-lg font-medium text-black'> Task Manager </h2>
-//         {children}
-//       </div>
+const AuthLayout = ({children}) => {
+  return (
+    <div className='flex h-screen'>
+      <div className='w-screen md:w-3/5 px-12 pt-8 pb-12'>
+        <h2 className='text-2lg font-bold text-black'> Task Manager </h2>
+        {children}
+      </div>
 
-//       <div className="hidden md:flex w-2/5 h-screen items-center justify-center bg-blue-50 bg-[url('/bg-img.png')] bg-cover bg-no-repeat bg-center overflow-hidden p-8">
-//         <img src={UI_IMG} className='w-[80%] max-w-md'/>
-//       </div>
-//     </div>
-//   )
-// }
+      <div className="hidden md:flex w-2/5 h-screen items-center justify-center bg-blue-50 bg-[url('/bg-img.png')] bg-cover bg-no-repeat bg-center overflow-hidden p-8">
+        <img src={UI_IMG} className='w-[80%] max-w-md'/>
+      </div>
+    </div>
+  )
+}
 
-// export default AuthLayout
+export default AuthLayout
